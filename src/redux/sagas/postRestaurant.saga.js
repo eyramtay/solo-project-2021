471eddc8d9e29ea5a480b5c9b45b12f207d07578
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-31 15:33:13 -0500

    created a saga for POST-ing to database

---
+++ src/redux/sagas/postRestaurant.saga.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+function* postRestaurant(action) {
+    try {
+        yield axios.post('/api/restaurant', {restaurant_name: action.restaurant_name,
+             cuisine: action.cuisine, restaurant_url: action.restaurant_url,
+            restaurant_bio: action.restaurant_bio });
+    } catch (error) {
+        console.log('Error POST-ing restaurant', error);
+    }
+}
+
+export default postRestaurant;
