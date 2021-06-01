5bedeea53b0c8be94fc195ba3c5391f95cd0cd7b
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-06-01 11:41:16 -0500

    imported necessary React frameworks

---
+++ src/components/RestaurantDetail/RestaurantDetail.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+
+
+function RestaurantDetail() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const params = useParams();
+
+    
+}
+
+export default RestaurantDetail;
