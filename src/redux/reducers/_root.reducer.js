90f8e3c8cf69bd7a26e48361b6902a1844c31608
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-06-03 03:05:10 -0500

    created a restaurantEdit reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import restaurantList from './restaurantList.reducer';
 import restaurantClicked from './restaurantClicked.reducer';
+import restaurantEdit from './restaurantEdit.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   restaurantList,
   restaurantClicked,
+  restaurantEdit,
 });
 
 export default rootReducer;
